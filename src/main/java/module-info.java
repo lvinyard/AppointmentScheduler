b88519301fdcf89c45e8951f3bef61561b7e3a1b
lvinyard <lvinyard98@gmail.com>
2023-02-12 23:06:27 -0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module vinyard.appointmentscheduler {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens vinyard.appointmentscheduler to javafx.fxml;
+    exports vinyard.appointmentscheduler;
+}
